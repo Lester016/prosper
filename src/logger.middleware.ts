9609664bc5059cddd3610217242a38e3f7735b78
Lester <lesterdeguzman1999@gmail.com>
2024-06-10 08:47:03 +0800

    Create logger middleware

---
+++ src/logger.middleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+
+export function loggerMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  console.log(
+    '\x1b[1m\x1b[36m%s', // Logs will be cyan and bold.
+    'Start of',
+    req.method,
+    req.url,
+    '\x1b[1m\x1b[37m', // Next logs will be white and bold.
+  );
+
+  // Only logs the end if there was no error encountered.
+  res.on('finish', () => {
+    // Status 400 means there was an error in response.
+    if (res.statusCode < 400) {
+      console.log('\x1b[1m\x1b[33m%s', 'End of', req.method, req.url);
+    }
+  });
+  next();
+}
