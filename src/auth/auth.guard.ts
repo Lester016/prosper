f91038243af3520e14f0b103a0327c0717322b26
Lester <lesterdeguzman1999@gmail.com>
2024-07-04 13:24:43 +0800

    feat: Implement auth guard(require access token for sensitive endpoint)

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,38 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { IS_PUBLIC_KEY } from './auth.decorator';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      // 💡 See this condition
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization?.split(' ')[1] ?? [];
+
+    try {
+      const user = await this.jwtService.verifyAsync(token);
+      request.user = user;
+    } catch (error) {
+      throw new HttpException('Unauthorized access', HttpStatus.UNAUTHORIZED);
+    }
+
+    return true;
+  }
+}
